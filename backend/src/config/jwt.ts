8fe681ddb9a65c51a7ef6aac13265467d47dde3c
Ibrahim Elaradi <ibrahimelaradi@gmail.com>
2024-08-04 01:44:40 +0200

    tweak: adjust access token expiry

--- backend/src/config/jwt.ts
+++ backend/src/config/jwt.ts
@@ -1,6 +1,6 @@
 export const jwtConfig = {
   accessSecret: process.env.JWT_ACCESS_SECRET || "SomeAccessSecret123!",
-  accessExpresIn: process.env.JWT_ACCESS_EXPIRE || "1m",
+  accessExpresIn: process.env.JWT_ACCESS_EXPIRE || "5m",
   refreshSecret: process.env.JWT_REFRESH_SECRET || "SomeRefreshSecret123!",
   refreshExpresIn: process.env.JWT_REFRESH_EXPIRE || "7d",
 };
